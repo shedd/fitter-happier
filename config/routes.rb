c914e6164fca365c6738f087e52b45a186837ed9
Thibaut Barrère <thibaut.barrere@gmail.com>
2013-06-04 22:43:20 +0200

    Restrict actions to GET verb
    Rails 4.0 requires that routes using match must specify the request
    method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match '/fitter_happier' => 'fitter_happier#index'
-  match '/fitter_happier/site_check' => 'fitter_happier#site_check'
-  match '/fitter_happier/site_and_database_check' => 'fitter_happier#site_and_database_check'
+  match '/fitter_happier' => 'fitter_happier#index', :via => :get
+  match '/fitter_happier/site_check' => 'fitter_happier#site_check', :via => :get
+  match '/fitter_happier/site_and_database_check' => 'fitter_happier#site_and_database_check', :via => :get
 end
